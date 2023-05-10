4b444627c0fbe3fe1fc8c312f8fa0583863db3a6
arianshnsz <arianshnsz@gmail.com>
2023-05-10 20:44:28 +0330

    flashcard model is there

--- flashcard/models.py
+++ flashcard/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Deck(models.Model):
+    title = models.CharField(max_length=50)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+class Card(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    back = models.CharField(max_length=250)
+    date_created = models.DateTimeField(auto_now_add=True)
+    learned = models.BooleanField(default=False)
+    deck = models.ForeignKey("Deck", on_delete=models.CASCADE)
+    number_of_practice_needed = models.IntegerField(default=5, editable=False)
+
+    def __str__(self) -> str:
+        return self.title
